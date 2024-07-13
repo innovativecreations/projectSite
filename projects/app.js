8f445af171b23f4d1f35a01df2cd174578291a3a
Mayank <innovativecreations195@gmail.com>
2024-07-13 22:38:23 +0530

    project view basic structure

---
+++ projects/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const projects = [
+    { title: "M", description: "Descroption" },
+];
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
