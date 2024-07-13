53e123faece94085b92621a47f117bd5ccd99baa
Mayank <innovativecreations195@gmail.com>
2024-07-13 22:50:30 +0530

    making cards

--- projects/app.js
+++ projects/app.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', { projects });
 });
 
 app.listen(port, () => {
