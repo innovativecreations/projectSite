a5a11df9952730ccb66e5675347aae883489d282
Mayank <innovativecreations195@gmail.com>
2024-07-14 00:28:19 +0530

    designing the cards

--- projects/app.js
+++ projects/app.js
@@ -3,7 +3,8 @@ const app = express();
 const port = 3000;
 
 const projects = [
-    { title: "M", description: "Descroption" },
+    { title: "First Project", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultricies nisl a enim consequat mattis. Proin tempor ullamcorper justo. Nulla eu maximus lorem. Aenean eu interdum diam. Quisque cursus urna id orci rutrum, at convallis sem congue. Curabitur sit amet est nisl. Quisque convallis tempus est ut maximus. Vivamus vita", thumbnail: "https://dummyimage.com/600x400/000/fff" },
+    { title: "Second Project", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultricies nisl a enim consequat mattis. Proin tempor ullamcorper justo. Nulla eu maximus lorem. Aenean eu interdum diam. Quisque cursus urna id orci rutrum, at convallis sem congue. Curabitur sit amet est nisl. Quisque convallis tempus est ut maximus. Vivamus vita", thumbnail: "https://dummyimage.com/600x400/000/fff" },
 ];
 
 app.set('view engine', 'ejs');
